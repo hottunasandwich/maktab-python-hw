ba29a8a226bd3dd3715261470d37ff37757c63f3
hottunasandwich <rodrik.v@gmail.com>
2020-09-25 11:47:07 +0330

    removed commented lines

--- 5/1.py
+++ 5/1.py
@@ -1,7 +1,6 @@
 from functools import total_ordering
 
 
-
 @total_ordering
 class Mylist(list):
     def __eq__(self, other):
@@ -11,18 +10,12 @@ class Mylist(list):
         return (sum(self) / len(self)) < (sum(other) / len(other))
 
 
-# li = MyList([1, 2, 3, 4, 5])
-
-# li2 = MyList([1, 1, 1, 1])
-
-
 li = [[1, 2], [2.1, 5.4, 3], [1, 4], [100, 100, 100], [90, 90, 90, 90, 90], [0, 100, 100]] 
 li = [Mylist(item) for item in li] 
 print(sorted(li))
 
 
 class Mylist2(list):
-
     def __eq__(self, other):
         return Mylist.__eq__(self, other)
 
@@ -42,10 +35,10 @@ class Mylist2(list):
         return sum(self) / len(self) >= sum(other) / len(other)
 
 
-l1 = Mylist2([0, 100, 100]) #66.66666666666667
-l2 = Mylist2([101, 0]) #50.5
-l3 = Mylist2([100, 0, 100]) #66.66666666666667
-print('1:', l1 < l2) #66.66666666666667 < #50.5
+l1 = Mylist2([0, 100, 100])
+l2 = Mylist2([101, 0])
+l3 = Mylist2([100, 0, 100])
+print('1:', l1 < l2)
 print('2:', l1 > l2)
 print('4:', l1 <= l2) 
 print('5:', l1 >= l2) 
